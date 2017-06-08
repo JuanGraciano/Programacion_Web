2b0df1c34628225e64d5ffb99a12de5e0df0dacb
JuanGraciano <juanpedro7juanpedro7@gmail.com>
2017-06-08 03:17:58 -0400

    adding handlebars

---
+++ App/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars');
+var helpers = require('./lib/helpers');
+
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.engine('handlebars', 
+			exphbs({defaultLayout: 'main', 
+			helpers:helpers,
+			layoutsDir: __dirname + '/views/layouts/',
+			partialsDir: __dirname +  '/views/partials/'
+			})
+);
+
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	console.log(__dirname)
+  	 res.render('home');
+  });
+
+app.get('/about', function (req, res) {
+  	res.render('about');
+  });
+
+var port = 8080
+
+app.listen(port, function () {
+  console.log('App listening on port ', port);
+});
