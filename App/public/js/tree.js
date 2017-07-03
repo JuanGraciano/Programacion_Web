02643554ce4ecabbdeeaa964ad5319f027480d1b
JuanGraciano <juanpedro7juanpedro7@gmail.com>
2017-07-03 16:48:12 -0400

    Fix CSS

--- App/public/js/tree.js
+++ App/public/js/tree.js
@@ -2,10 +2,10 @@
 var tree = '';
 
 function head(){
-	tree = '<ul class="demo">';
-	tree += '<li class="root"> ' + document.body.tagName + '</li>';
+	tree = '<ul class="dad">';
+	tree += '<li> ' + document.body.tagName + '</li>';
 	for(var i = 0; i < document.body.children.length; i++){
-		tree += '<ul>';
+		tree += '<ul class="root">';
 		headChild(document.body.children, i);
 		tree += '</ul>';
 	}
@@ -16,13 +16,13 @@ function head(){
 function headChild(tag, i)
 {
     if(tag[i].children.length > 0){
-    	tree += '<il class="hasChild"> ' + tag[i].tagName + '</li>';
+    	tree += '<li> ' + tag[i].tagName + '</li>';
     }
     else if(tag[i].children.length == 0){
-    	tree += '<il class="child"> ' + tag[i].tagName + '</li>';
+    	tree += '<li> ' + tag[i].tagName + '</li>';
     }
 	for(var j = 0; j < tag[i].children.length; j++) {
-		tree += '<ul>';	
+		tree += '<ul class="pages">';	
 		headChild(tag[i].children, j);
 		tree += '</ul>';
 	}
