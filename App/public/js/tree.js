a5351026acab30ca92633b1b36631d031580040d
JuanGraciano <juanpedro7juanpedro7@gmail.com>
2017-07-03 00:48:53 -0400

    Playing with css

--- App/public/js/tree.js
+++ App/public/js/tree.js
@@ -2,22 +2,24 @@
 var tree = '';
 
 function head(){
-	tree = '<ul class="demo">';
+	tree = '<ul class="root">';
 	tree += '<li> ' + document.body.tagName + '</li>';
 	for(var i = 0; i < document.body.children.length; i++){
-		tree += '<ul>';
+		tree += '<ul >';
 		headChild(document.body.children, i);
 		tree += '</ul>';
 	}
 	tree += '</ul>';
 	document.getElementById("head").innerHTML = tree;
-	//document.write(tree);
 }
 
 function headChild(tag, i)
 {
-    if(tag[i].children.length >= 0){
-    	tree += '<il> ' + tag[i].tagName + '</li>';
+    if(tag[i].children.length > 0){
+    	tree += '<il class="hasChild"> ' + tag[i].tagName + '</li>';
+    }
+    else if(tag[i].children.length == 0){
+    	tree += '<il class="child"> ' + tag[i].tagName + '</li>';
     }
 	for(var j = 0; j < tag[i].children.length; j++) {
 		tree += '<ul>';	
